b5865608b67c359bfea982761396993f5ac256df
zione-kushwaha <jeevankushwaha461@gmail.com>
2026-01-12 01:28:27 +0545

    Remove broken flutter submodule

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3620367788"
+    serviceWorkerVersion: "3581761333"
   }
 });
